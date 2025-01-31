070bad583c30c46d7fe382bcb0d45c360c3b5932
Mark McArthey <mcarthey@gmail.com>
2025-01-30 21:46:13 -0600

    Added W2 in-class code

---
+++ W2_EnhancedFileIO/Program.cs
@@ -0,0 +1,40 @@
+﻿using W2_EnhancedFileIO.Services;
+
+namespace W2_EnhancedFileIO
+{
+
+    // Displaying options for user
+    // Reading from file
+    // Writing to file
+    // Parsing file into array
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1. Display Characters");
+            Console.WriteLine("2. Add Character");
+            Console.WriteLine("3. Level Up Character");
+
+            var menuOption = Console.ReadLine();
+
+            if (menuOption == "1")
+            {
+                CharacterManager.DisplayCharacters();
+            }
+
+            if (menuOption == "2")
+            {
+                CharacterManager.AddCharacter("Jane");
+            }
+
+            if (menuOption == "3")
+            {
+
+            }
+        }
+
+
+    }
+}
---
+++ W2_EnhancedFileIO/Services/CharacterManager.cs
@@ -0,0 +1,67 @@
+﻿namespace W2_EnhancedFileIO.Services
+{
+    public class CharacterManager
+    {
+        public static void DisplayCharacters()
+        {
+            var lines = File.ReadAllLines("Files/input.csv");
+
+            var header = lines[0];
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string name = "";
+                string job = "";
+                string level = "";
+                string hitPoints = "";
+                string equipment = "";
+
+                // does the line start with a " ?
+                var firstQuoteLocation = lines[i].IndexOf('"');
+
+                // if " is not found
+                if (firstQuoteLocation < 0)
+                {
+                    var cols = lines[i].Split(",");
+
+                    name = cols[0];
+                    job = cols[1];
+                    level = cols[2];
+                    hitPoints = cols[3];
+                    equipment = cols[4];
+                    //var items = equipment.Split("|");
+
+                }
+                else
+                {
+                    lines[i] = lines[i].TrimStart('"');
+                    var parts = lines[i].Split('"');
+                    var firstPart = parts[0];
+                    var secondPart = parts[1].Substring(1);
+                    
+                    var cols = secondPart.Split(",");
+                    name = firstPart;
+                    job = cols[0];
+                    level = cols[1];
+                    hitPoints = cols[2];
+                    equipment = cols[3];
+                }
+
+                Console.WriteLine($"Name: {name}");
+                Console.WriteLine($"Job: {job}");
+                Console.WriteLine($"Level: {level}");
+                Console.WriteLine($"Hit Points: {hitPoints}");
+                Console.WriteLine($"Equipment: {equipment}");
+                Console.WriteLine("-----------");
+            }
+        }
+
+        public static void AddCharacter(string name)
+        {
+            using (StreamWriter writer = new StreamWriter("input.csv", true))
+            {
+                writer.WriteLine(name);
+            }
+        }
+    }
+}
